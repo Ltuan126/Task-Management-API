cfeef424b31f8f659befca019b0bc54e56374d38
Tuan <tuanlenguyen612@gmail.com>
2026-03-12 22:15:10 +0700

    feat(tasks): implement task controller

---
+++ backend-clean-api/src/modules/tasks/task.controller.js
@@ -0,0 +1,31 @@
+const taskService = require("./task.service");
+const taskController = require("./taskController");
+
+class TaskController {
+    async createTask(req, res) {
+        const task = await taskService.creataTask(req.body);
+        res.status(201).json(task);
+    }
+
+    async getTasks(req, res) {
+        const tasks = await taskService.getTasks();
+        res.json(tasks);
+    }
+
+    async getTask(req, res) {
+        const task = await taskService.getTask(req.params.id)
+        res.json(task);
+    }
+
+    async updateTask(req, res) {
+        const task = await taskService.updateTask(req.params.id, req.body)
+        res.json(task);
+    }
+
+    async deleteTask(req, res) {
+        const task = await taskService.deleteTask(req.params.id)
+        res.json({ message: "Task deleted" });
+    }
+}
+
+module.exports = new TaskController();
