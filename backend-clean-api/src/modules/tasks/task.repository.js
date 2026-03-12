a675930e7a048253fdd8546f70061924dae0f4fd
Tuan <tuanlenguyen612@gmail.com>
2026-03-12 22:13:31 +0700

    feat(tasks): implement task repository layer

---
+++ backend-clean-api/src/modules/tasks/task.repository.js
@@ -0,0 +1,25 @@
+const Task = require("./task.model");
+
+class TaskRepository {
+    async createTask(data) {
+        return await Task.create(data);
+    }
+
+    async getAllTask(data) {
+        return await Task.find();
+    }
+
+    async getTaskbyId(id) {
+        return await Task.findById(id);
+    }
+
+    async updateTask(id, data) {
+        return await Task.findByIdAndUpdate(id, data, { new: true });
+    }
+
+    async deleteTask(id) {
+        return await Task.findByIdAndDelete(id);
+    }
+}
+
+module.exports = new TaskRepository();
