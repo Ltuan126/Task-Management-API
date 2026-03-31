4f6f4b729d5172fcb52b268b7ba4ff15472f02ad
Tuan <tuanlenguyen612@gmail.com>
2026-03-31 22:56:28 +0700

    fix: correct user pre-save password hashing hook

--- backend-clean-api/src/models/user.model.js
+++ backend-clean-api/src/models/user.model.js
@@ -28,14 +28,13 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password only when it is newly set or changed.
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
     if (!this.isModified("password")) {
-        return next();
+        return;
     }
 
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
