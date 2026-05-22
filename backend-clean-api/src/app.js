203ceed1465459ed5c67136c4e665a504a91f9e7
Tuan <tuanlenguyen612@gmail.com>
2026-05-22 22:35:21 +0700

    fix: manual CORS with app.options for Express 5 compatibility

--- backend-clean-api/src/app.js
+++ backend-clean-api/src/app.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app = express();
 const { swaggerSpec, swaggerMiddleware } = require("./config/swagger");
-const cors = require("cors");
+// Manual CORS - Express 5 intercepts OPTIONS before cors middleware
+app.options("*", (req, res) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.status(204).end();
+});
 
-app.use(cors({
-    origin: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-}));
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 app.use(express.json());
 
